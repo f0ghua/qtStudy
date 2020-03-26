cca9f387f880c501fbaf7dcfcc23399934862ebe
fog <hualiang@sapaauto.com>
2020-03-26 16:09:02 +0800

    ! update crc test

--- qtStudy_multiThread/main.cpp
+++ qtStudy_multiThread/main.cpp
@@ -7,7 +7,7 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
 
     QAppLogging::installHandler();
-    QAppLogging::instance()->setOutputDest(QAppLogging::eDestFile);
+    //QAppLogging::instance()->setOutputDest(QAppLogging::eDestFile);
 
     MainWindow w;
     w.show();
